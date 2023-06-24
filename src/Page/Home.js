35e3ab58e162edcd31412fb6c4dda83410d5b788
balamurugan110799 <balamurugan99711@gmail.com>
2023-06-24 23:41:16 +0530

    leaf dynamic

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,35 +1,35 @@
 import React from 'react'
 import Leaf from "../components/Assets/Images/leaf.webp"
 import { Link } from "react-router-dom";
+import Header from '../components/JS/Header';
 
 function Home() {
   return (
     <div>
+      <Header />
       <div className='container mx-auto py-10'>
-        <div className='grid grid-cols-4 gap-4'>
-            <div className='col-span-1'>
-              <div className='border p-4'>
-              <img src={Leaf} alt="leaf"/>
-              <p className='text-text-color pt-2 text-ash-blue heading text-[20px]'>Green leaf </p>
-
-              <div className='grid grid-cols-2 pt-2'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 mdsm:grid-cols-2  sm:grid-cols-1 gap-4'>
+          <div className='col-span-1'>
+            <div className='border group p-4'>
+              <div className='overflow-hidden'>
+                <Link to="/leaf">
+                  <img src={Leaf} alt="leaf" className='group-hover:scale-125 duration-700 cursor-pointer' />
+                </Link>
+              </div>
+              <p className='text-text-color py-2 pb-1 text-primary group-hover:text-primary heading text-[24px]'>Green leaf </p>
+              <div className='grid grid-cols-2 pt-1'>
                 <div className='flex justify-start'>
-                    <button className='bg-primary hover:bg-ash-blue duration-300 px-10 py-2 rounded-lg text-white'>Code</button>
+                  <button className='bg-primary hover:bg-ash-blue duration-300 px-10 py-2 border-design text-white'>Code</button>
                 </div>
                 <div className='flex justify-end'>
                   <Link to="/leaf">
-                  
-                    <button className='bg-primary px-10 py-2 rounded-lg text-white'>View</button></Link>
+                    <button className='bg-primary hover:bg-ash-blue duration-300 px-10 py-2 border-design text-white'>View</button></Link>
                 </div>
-
               </div>
-              </div>
-
             </div>
-
+          </div>
         </div>
       </div>
-        
     </div>
   )
 }
--- src/Page/Leaf/Leaf.js
+++ src/Page/Leaf/Leaf.js
@@ -1,13 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Leaf.css"
 
 function Leaf() {
+    const [sessions, setSessions] = useState({ sessions: "summer" })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setSessions({
+            ...sessions,
+            [name]: value,
+        })
+    }
+    console.log(sessions)
+    useEffect(() => {
+        document.getElementById("summer").checked = true;
+        console.log(sessions)
+    }, [])
+
+
+
     return (
-        <div class="background">
-            <div class="leaf-head">
-                <div class="left-leaf"></div>
-                <div class="leaves">
-                    <div class="left-right"></div>
+        <div className={`${sessions.sessions === "summer" ? "background-green" : sessions.sessions === "winter" ? "backround-winter" : sessions.sessions === "spring" ? "background-spring" : sessions.sessions === "autumn" ? "background-autumn" : " "} background relative`}>
+            <div className="leaf-head">
+                <div className={`${sessions.sessions === "summer" ? "summer-left-leaf" : sessions.sessions === "winter" ? "winter-left-leaf" : sessions.sessions === "winter" ? "winter-left-leaf" : sessions.sessions === "spring" ? "spring-left-leaf" : sessions.sessions === "autumn" ? "autumn-left-leaf" : ""} left-leaf`}></div>
+                <div className={`${sessions.sessions === "summer" ? "summer-leaves" : sessions.sessions === "winter" ? "winter-leaves" : sessions.sessions === "spring" ? "spring-leaves" : sessions.sessions === "autumn" ? "autumn-leaves" : ""} leaves`}>
+                    <div className={`${sessions.sessions === "summer" ? "summer-right-leaf" : sessions.sessions === "winter" ? "winter-right-leaf" : sessions.sessions === "spring" ? "spring-right-leaf" : sessions.sessions === "autumn" ? "autumn-right-leaf" : ""} right-leaf`}></div>
+                </div>
+            </div>
+
+            <div className="absolute bottom-0 w-[100%] px-4">
+                <div className="grid py-4 lg:grid-cols-2 md:grid-cols-1 mdsm:grid-cols-1 sm:grid-cols-1">
+                    <div>
+
+                    </div>
+                    <div className="grid lg:grid-cols-4 md:grid-cols-2 mdsm:grid-cols-2 sm:grid-cols-1 gap-4">
+                        <div>
+                            <label className=" cursor-pointer">
+                                <div className="bg-primary hover:bg-ash-blue duration-300 px-4 py-2 border-design text-white">
+                                    <input id="summer" type="radio" value="summer" onChange={(e) => handleChange(e)} name="sessions" className="mr-2" />Summer
+                                </div>
+                            </label>
+                        </div>
+
+                        <div>
+                            <label className=" cursor-pointer">
+                                <div className="bg-primary hover:bg-ash-blue duration-300 px-4 py-2 border-design text-white">
+                                    <input type="radio" value="winter" onChange={(e) => handleChange(e)} name="sessions" className="mr-2" />Winter
+                                </div>
+                            </label>
+                        </div>
+                        <div>
+                            <label className=" cursor-pointer">
+                                <div className="bg-primary hover:bg-ash-blue duration-300 px-4 py-2 border-design text-white">
+                                    <input type="radio" value="spring" onChange={(e) => handleChange(e)} name="sessions" className="mr-2" />Spring
+                                </div>
+                            </label>
+                        </div>
+                        <div>
+                            <label className=" cursor-pointer">
+                                <div className="bg-primary hover:bg-ash-blue duration-300 px-4 py-2 border-design text-white">
+                                    <input type="radio" value="autumn" onChange={(e) => handleChange(e)} name="sessions" className="mr-2" />Autumn
+                                </div>
+                            </label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
